a46c2d66df6c34c98c9d17b52eaabb804153279e
ChronicleGremlin <alecjost7@gmail.com>
2024-04-11 12:29:35 -0500

    Added comments

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,10 @@
-   // This demo shows off comments!
+   // This demo shows off comments! Wowww comments 
 
    // console.log("This does not print.");
+
+   /* This is another comment
+   I can write comments on 
+   lots of lines */
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -8,5 +12,6 @@
    to have
    multi-line
    comments. */
+ // this won't print// 
 
    console.log("Comments make your code more readable by others.");
