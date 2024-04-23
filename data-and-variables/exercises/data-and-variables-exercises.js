a530d35c246031cbabf512a8660e6bea9f8efdc0
ChronicleGremlin <alecjost7@gmail.com>
2024-04-23 13:41:31 -0500

    days to mars

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,8 +1,23 @@
-// Declare and assign the variables below
+let shuttleName = 'Determination' ;
+let shuttleSpeed = 17500 ;
+let distanceToMars = 225000000 ;
+let distanceToMoon = 384400 ;
+const milesPerKm = 0.621 ;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
 // Calculate a space mission below
+let milesToMars =
+distanceToMars * milesPerKm;
+console.log(milesToMars)
+
+let hoursToMars =
+milesToMars / shuttleSpeed;
+console.log(hoursToMars)
+
+let daysToMars =
+hoursToMars / 24;
+console.log(shuttleName + " will take  " + daysToMars + " days to reach Mars.");
 
 // Print the results of the space mission calculations below
 
