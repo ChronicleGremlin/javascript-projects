60c65865e56cc1e4e313fda6ffcdedf64cc827fa
ChronicleGremlin <alecjost7@gmail.com>
2024-04-24 10:19:42 -0500

    Trip to Moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -4,23 +4,33 @@ let distanceToMars = 225000000 ;
 let distanceToMoon = 384400 ;
 const milesPerKm = 0.621 ;
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
-
 // Calculate a space mission below
 let milesToMars =
 distanceToMars * milesPerKm;
-console.log(milesToMars)
+//console.log(milesToMars)
 
 let hoursToMars =
 milesToMars / shuttleSpeed;
-console.log(hoursToMars)
+//console.log(hoursToMars)
 
 let daysToMars =
 hoursToMars / 24;
-console.log(shuttleName + " will take  " + daysToMars + " days to reach Mars.");
 
 // Print the results of the space mission calculations below
+console.log(shuttleName + " will take  " + daysToMars + " days to reach Mars.");
 
 // Calculate a trip to the moon below
 
+let milesToMoon =
+distanceToMoon * milesPerKm;
+//console.log(milesToMoon);
+
+let hoursToMoon =
+milesToMoon / shuttleSpeed;
+//console.log(hoursToMoon);
+
+let daysToMoon =
+hoursToMoon / 24;
+
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
