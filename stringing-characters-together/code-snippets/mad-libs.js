93ef1764fe244dae624dfacb9b644491bd04d00e
ChronicleGremlin <alecjost7@gmail.com>
2024-05-06 13:40:16 -0500

    Mad libs

--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = 'strawberries';
+let name = 'Alec';
+let verb = 'swim';
+let adjective = 'dazzling';
+let color = 'sage green';
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
