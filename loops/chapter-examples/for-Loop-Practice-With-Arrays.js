896b2ef2788ef3487e7fe7da9e3a03e91b1affab
ChronicleGremlin <alecjost7@gmail.com>
2024-05-14 12:15:21 -0500

    Chapter examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
-
+const nameList = ['Alec', 'Jost'];
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < nameList.length; i++){
+    console.log(nameList[i]);
+}
