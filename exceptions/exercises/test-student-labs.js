f4d301b05c989cb926885303f668f1de272626c3
ChronicleGremlin <alecjost7@gmail.com>
2024-06-25 20:01:24 -0500

    Exeptions chapter examples and exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,29 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
-  }
+    try {
+      console.log(`${lab.student} code worked: ${result === 27}`);
+      let result = lab.runLab(3);
+    } catch(err) {
+      let result = "Error thrown";
+      console.log(result);
+    }
+}
 }
 
-let studentLabs = [
+let studentLabs2 = [
   {
     student: 'Carly',
     runLab: function (num) {
-        return Math.pow(num, num);
+      return Math.pow(num, num);
     }
   },
   {
     student: 'Erica',
     runLab: function (num) {
-        return num * num;
+      return num * num;
     }
   }
 ];
 
-gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
