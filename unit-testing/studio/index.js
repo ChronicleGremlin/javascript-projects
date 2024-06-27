7009f99c5d74552c170db3b2deb81637eed4fe8d
ChronicleGremlin <alecjost7@gmail.com>
2024-06-27 00:15:24 -0500

    Unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,11 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function() {
+  },
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,26 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function() {
 
   // Write your unit tests here!
-  
+  test("Returns value of organization in object", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  })
+
+  test("Returns value of executiveDirector in object", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  })
+
+  test("Returns value of percentageCoolEmployees in object", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  })
+
+  test("Returns value of programsOffered in object", function() {
+    expect(launchcode.programsOffered).toBe("Web Development", "Data Analysis", "Liftoff");
+  })
+
+  test("When passed a number that is ONLY divisible by 2,", function() {
+    expect(launchcode.launchOutput()).toBe("Launch!");
+  })
 });
